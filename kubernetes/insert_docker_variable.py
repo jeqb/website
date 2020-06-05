948b344e135d53583dfdfc122be812f9e6f8a1c1
James Bonner <jameseqbonner@gmail.com>
2020-06-04 22:27:55 -0700

    added build pipeline and kube folder

---
+++ kubernetes/insert_docker_variable.py
@@ -0,0 +1,22 @@
+import os
+import argparse, sys
+
+parser=argparse.ArgumentParser()
+
+# argument for imagePullSecrets
+parser.add_argument('--s')
+cli_args = parser.parse_args().__dict__
+
+# get the secret value
+registry_secret = cli_args.get('s')
+
+# open the yaml file
+with open('./Backend.Dockerfile', 'r') as myfile:
+	yaml_file = myfile.read()
+	myfile.close()
+
+# insert the variable
+yaml_file = yaml_file.replace('%', registry_secret)
+new_file = open('./Backend.Dockerfile', 'w')
+new_file.write(yaml_file)
+new_file.close()
