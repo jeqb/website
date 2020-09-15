c35411562be2c2438c7ad91e943fd11d56778905
James Bonner <jameseqbonner@gmail.com>
2020-09-14 19:42:40 -0700

    custom Text editor is working w/ images

--- client-side/src/components/blog/BlogEditor.js
+++ client-side/src/components/blog/BlogEditor.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const BlogEditor = () => {
+  const targetNode = useRef(null)
+
+  useEffect(() => {
+    window.ClassicEditor.create(targetNode.current,
+      {
+				
+				toolbar: {
+					items: [
+						'heading',
+						'|',
+						'bold',
+						'italic',
+						'link',
+						'bulletedList',
+						'numberedList',
+						'|',
+						'indent',
+						'outdent',
+						'|',
+						'imageUpload',
+						'blockQuote',
+						'insertTable',
+						'mediaEmbed',
+						'undo',
+						'redo'
+					]
+				},
+				language: 'en',
+				image: {
+					toolbar: [
+						'imageTextAlternative',
+						'imageStyle:full',
+						'imageStyle:side'
+					]
+				},
+				table: {
+					contentToolbar: [
+						'tableColumn',
+						'tableRow',
+						'mergeTableCells'
+					]
+				},
+				licenseKey: '',
+				
+			} )
+			.then( editor => {
+				window.editor = editor;
+			} )
+			.catch( error => {
+				console.error( 'Oops, something went wrong!' );
+				console.error( 'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:' );
+				console.warn( 'Build id: 9x3ve0vzsv96-8o65j7c6blw0' );
+				console.error( error );
+			} );
+  }, [])
+
   return (
-    <div>
-      THIS IS A DEV COMPONENT
-    </div>
+    <div ref={targetNode}></div>
   );
 }
 
